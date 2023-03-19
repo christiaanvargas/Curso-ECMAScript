ffa2ef71a9bf75e0683d3d014e570e8cb9ffb853
Christiaanvargas <56331682+christiaanvargas@users.noreply.github.com>
2023-03-19 07:44:39 -0500

    Difeinicion de let, const y arror function.

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,13 @@
+// función de tipo standard
+
+function square(num) {
+    return num * num;
+}
+
+// función de flecha (Arror funtion)
+const square = (num) => {
+    return num * num;
+}
+
+// funtion fecha con return implicito
+const square = num => num * num;
