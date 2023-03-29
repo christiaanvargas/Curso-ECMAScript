f43affa971e5f75fdb537bdb76c0f290952a8872
Christiaan Vargas <56331682+christiaanvargas@users.noreply.github.com>
2023-03-28 19:34:35 -0500

    📝 Cambios archivo 00-flat-map.js
    Se eliminó las anotaciones dentro del archivo y se agregó el ejemplo uso del flatMap al mismo.

--- src/es10/00-flat-map.js
+++ src/es10/00-flat-map.js
@@ -1,5 +1,8 @@
+// flat
+
 const array = [1, 1, 2, 3, 4, [1, 3, 5, 6, [1, 2, 4]]];
 console.log(array.flat(3));
 
-/* const arrayMuestra = [11, 12, 13, 14, 15, 16, 17, 18, 19, [21, 23, 25, 26, [31, 32, 34, 33, 35, 37]]];
-console.log(arrayMuestra.flat(3)); */
+// flatmap
+const array2 = [1, 2, 3, 4, 5];
+console.log(array2.flatMap(v => [v, v * 2]));
