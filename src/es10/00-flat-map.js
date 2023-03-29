e82e8f9f3267b219fcb5d00379030be31dcc6cc2
Christiaan Vargas <56331682+christiaanvargas@users.noreply.github.com>
2023-03-28 19:21:43 -0500

    Se incluye fla map
    Se crea un archivo 00-flat-map.js con los conceptos de flat map que están presnte en la  actualización ES10.

---
+++ src/es10/00-flat-map.js
@@ -0,0 +1,5 @@
+const array = [1, 1, 2, 3, 4, [1, 3, 5, 6, [1, 2, 4]]];
+console.log(array.flat(3));
+
+/* const arrayMuestra = [11, 12, 13, 14, 15, 16, 17, 18, 19, [21, 23, 25, 26, [31, 32, 34, 33, 35, 37]]];
+console.log(arrayMuestra.flat(3)); */
